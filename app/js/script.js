3daf660723666eacca8386942149dbbaaee5f5c9
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-03-13 14:26:28 +0530

    feat/getting data from api

--- app/js/script.js
+++ app/js/script.js
@@ -15,3 +15,18 @@ refresh_btn.addEventListener('click', function(){
 remove.addEventListener('click', function(){
   console.log(remove);
 })
+
+// ########### API ###########
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api2.binance.com/api/v3/ticker/24hr', true);
+request.send();
+request.onload = function() {
+  if(request.status != 200) {
+    console.log(`Error ${request.status}: ${request.statusText}`);
+  } else {
+    let data = JSON.parse(request.response);
+    data.forEach(crypto => {
+      console.log(crypto.symbol);
+    });
+  }
+}
