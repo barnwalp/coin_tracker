63f85fd1184e64da5ba2ab7190abeaedf3cd660a
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-03-14 09:10:49 +0530

    feat/populating contents from api using javascript

--- app/js/script.js
+++ app/js/script.js
@@ -14,34 +14,38 @@ request.onload = function() {
     console.log(`Error ${request.status}: ${request.statusText}`);
   } else {
     data = JSON.parse(request.response);
-    // data.forEach(crypto => {
-    //   console.log(crypto.symbol);
-    // });
+    // Adding data to the container using javascript
+    data.forEach(crypto => {
+      const coin = document.createElement('div');
+      coin.setAttribute('class', 'coin');
+
+      const left = document.createElement('div');
+      left.setAttribute('class', 'left');
+
+      const coin_name = document.createElement('div');
+      coin_name.setAttribute('class', 'coin-name')
+      coin_name.textContent = crypto.symbol;
+
+      const coin_price = document.createElement('div');
+      coin_price.setAttribute('class', 'coin-price');
+      let price = `${crypto.lastPrice} - ${crypto.weightedAvgPrice}`
+      coin_price.textContent = price;
+      console.log(price);
+
+      const add_btn = document.createElement('button');
+      add_btn.setAttribute('class', 'btn add');
+      add_btn.textContent = 'Add';
+
+      search_items.append(coin);
+      coin.append(left);
+      left.append(coin_name);
+      left.append(coin_price);
+      coin.append(add_btn);
+    });
   }
 }
 // console.log(data);
 
-// Adding data to the container using javascript
-const coin = document.createElement('div');
-coin.setAttribute('class', 'coin');
-
-const left = document.createElement('div');
-left.setAttribute('class', 'left');
-
-const coin_name = document.createElement('div');
-coin_name.setAttribute('class', 'coin-name')
-
-const coin_price = document.createElement('div');
-coin_price.setAttribute('class', 'coin-price');
-
-const add_btn = document.createElement('button');
-add_btn.setAttribute('class', 'btn add');
-
-search_items.append(coin);
-coin.append(left);
-left.append(coin_name);
-left.append(coin_price);
-coin.append(add_btn);
 
 
 add_symbol.addEventListener('click', function(){
