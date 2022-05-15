3fcc1eb89908c47cf190577d5dd32d86b046fd0e
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-05-15 23:23:07 +0530

    feat/modularized display_coin function and added comments

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,14 @@
 let data;
-const display_container = document.querySelector('.portfolio');
-const refresh_btn = document.getElementById('refresh');
-const add_symbol = document.getElementById('add-symbol');
-const remove = document.querySelector('.remove');
-const search_items = document.querySelector('.search-items');
-const container = document.querySelector('.container');
+const portfolio = document.querySelector('.portfolio');
+const searched_coins = document.querySelector('.search-items');
+const search_container = document.querySelector('.search-container');
 const overlay = document.querySelector('.overlay');
 const search = document.getElementById('coin-search');
+
+// button references
+const refresh_btn = document.getElementById('refresh');
+const search_btn = document.getElementById('add-symbol');
+const remove_btn = document.querySelector('.remove');
 
 // ########### API ###########
 const load_coins = async () => {
@@ -14,13 +16,16 @@ const load_coins = async () => {
     const res = await fetch('https://api2.binance.com/api/v3/ticker/24hr');
     data = await res.json();
     console.log(data);
-    display_coin(data);
+    display_coin(data, searched_coins);
   } catch (err) {
     console.log(err)
   }
 };
 
-const display_coin = (coin_data) => {
+// creating html list of the cryptocurrencies with coin details
+// coin_data is the json data and container is the element to which 
+// json data is attached after being converted to html
+const display_coin = (coin_data, container) => {
   const htmlString = coin_data.map(coin => {
     return `
     <div class="coin">
@@ -32,18 +37,19 @@ const display_coin = (coin_data) => {
     </div>
     `
   }).join("");
-  search_items.innerHTML = htmlString;
+  container.innerHTML = htmlString;
 }
 
+// Implementing dynamic search in the searched coin section
 search.addEventListener('keyup', (e) => {
   const searchString = e.target.value.toLowerCase();
   const filtered_data = data.filter(coin => {
     return coin.symbol.toLowerCase().includes(searchString)
   });
-  display_coin(filtered_data);
+  display_coin(filtered_data, searched_coins);
 });
 
-if (search_items.firstChild) {
+if (searched_coins.firstChild) {
   const add_coins = document.querySelectorAll('.search-items .coin .add');
   console.log(add_coins);
   add_coins.forEach(function(add) {
@@ -54,17 +60,18 @@ if (search_items.firstChild) {
   });
 }
 
-add_symbol.addEventListener('click', function(){
-  if (container.classList.contains('hidden')) {
-    container.classList.remove('hidden');
+// Toggling the search coin overlay with search button
+search_btn.addEventListener('click', function(){
+  if (search_container.classList.contains('hidden')) {
+    search_container.classList.remove('hidden');
     overlay.classList.add('fade-in');
     overlay.classList.remove('fade-out');
-    add_symbol.textContent = ' Back ';
+    search_btn.textContent = ' Back ';
   } else {
-    container.classList.add('hidden');
+    search_container.classList.add('hidden');
     overlay.classList.remove('fade-in');
     overlay.classList.add('fade-out');
-    add_symbol.textContent = 'Search';
+    search_btn.textContent = 'Search';
   } 
 });
 
@@ -72,7 +79,7 @@ refresh_btn.addEventListener('click', function(){
   console.log(refresh_btn);
 });
 
-remove.addEventListener('click', function(){
+remove_btn.addEventListener('click', function(){
   console.log(remove);
 })
 
